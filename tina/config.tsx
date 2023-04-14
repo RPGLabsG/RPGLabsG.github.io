5236cf5373fbb0f9146d048b03981414fd9204fa
Jaka Bernard <jaka.bernard@kalmia.si>
2023-04-14 16:43:31 +0200

    Hopefully an env fix.

--- tina/config.tsx
+++ tina/config.tsx
@@ -10,6 +10,7 @@ const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID! || process.env.TINA_PUBLIC_CLIENT_ID!,
   branch:
     process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
+    process.env.PUBLIC_TINA_BRANCH! || // custom branch env override
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
     process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
