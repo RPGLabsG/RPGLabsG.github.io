ec36a13b3195aebff62bcf444235921413177470
Jaka Bernard <jaka.bernard@kalmia.si>
2023-04-17 15:30:03 +0200

    back to first solution.

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,13 @@
 import { Hero } from "../components/blocks/hero";
 import { Layout } from "../components/layout";
 
-export default function FourOhFour({ pageProps }) {
+export default function FourOhFour() {
   return (
     <Layout>
       <Hero
         data={{
           color: "default",
-          headline: "404 – Page Not Found" + JSON.stringify(pageProps),
+          headline: "404 – Page Not Found",
           text: "Oops! It seems there's nothing here, how embarrassing.",
           actions: [
             {
