29ebfa9ecc2a64d05c93f46d1ce097196c6907bd
bterpinc <borut.terpinc@kalmia.si>
2023-07-05 12:19:07 +0200

    Additional fixes

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = ({ rawData }) => {
     <footer
       className={`bg-gradient-to-br footer-main relative flex min-h-[240px] flex-col items-center justify-end `}
     >
-      <div className="font-sans flex justify-center pb-4">
+      <div className="font-sans flex justify-center pb-4 scale-75 sm:scale-100">
         <div className="flex">
         <KingdomLogo className="scale-75 sm:scale-100"></KingdomLogo>
         <small className="block self-center overflow-hidden pl-1 ml-2 text-sm text-white md:text-base">
@@ -17,7 +17,7 @@ export const Footer = ({ rawData }) => {
         </div>
         
       </div>
-      <div >
+      <div className="scale-75 sm:scale-100">
         <a
           href="https://the-kingdom.world/terms"
           className="font-sans sm:mx-6 mt-1 sm:mt-3 text-[10px] sm:text-xs text-white"
