e68fe827f00deadc22de00b4eb0f8b1759139620
bterpinc <borut.terpinc@kalmia.si>
2023-11-23 15:05:40 +0100

    Update google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,22 +10,24 @@ const App = ({ Component, pageProps }) => {
   }, []);
   return (
     <>
-      {/* Hotjar Tracking Code for Kingdom blo */}
+      {/* Google Tag manager */}
       <Script
         dangerouslySetInnerHTML={{
-          __html: `(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:3527485,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NQCZZRWJ');`,
         }}
       ></Script>
-      {/* Google tag (gtag.js) */}
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-L36PLLSPZ9"
-      ></Script>
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-L36PLLSPZ9');`,
-        }}
-      ></Script>
+      <div
+      dangerouslySetInnerHTML={{
+        __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQCZZRWJ"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                </noscript>`,
+      }}
+      />
+      
       <Component {...pageProps} />
     </>
   );
