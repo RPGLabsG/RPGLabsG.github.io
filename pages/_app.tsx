fa4fef9b98136914b4f9342ac60682d7253d83ec
bterpinc <borut.terpinc@kalmia.si>
2023-11-24 09:14:33 +0100

    Change GTM import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles.css";
 import { useRouter } from "next/router";
 import Script from "next/script";
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter();
@@ -11,15 +12,16 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       {/* Google Tag manager */}
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NQCZZRWJ');`,
-        }}
-      ></Script>
+      <Head>
+      <script dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NQCZZRWJ');`,
+        
+      }} />
+    </Head>
       <div
       dangerouslySetInnerHTML={{
         __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQCZZRWJ"
